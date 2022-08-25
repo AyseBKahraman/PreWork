02f844f53e9363eecf08ac6abd1e60830a1e5364
AyseBKahraman <110565692+AyseBKahraman@users.noreply.github.com>
2022-08-25 09:08:11 -0400

    midqueries

---
+++ FavoriteMovie/app.js
@@ -0,0 +1,20 @@
+const printToscreen = () => {
+    let ParentContainer = document.
+    querySelector('.ParentContainer')
+    ParentContainer.innerHTML = [`<img class= pic1 src="https://m.media-amazon.com/images/M/MV5BZmNiZmMzN2MtOTNkMy00YjA1LTg4NzktMWI4Njg1NzI3ODAyXkEyXkFqcGdeQXVyNzg5MzIyOA@@._V1_UX214_CR0,0,214,317_AL_.jpg">`,
+    `<img class= "pic1" src="https://m.media-amazon.com/images/M/MV5BMTQ3OTY0ODk0M15BMl5BanBnXkFtZTYwNzE4Njc4._V1_UY317_CR7,0,214,317_AL_.jpg"> `]
+}
+
+const printToscreen2 = () => {
+    let ParentContainer = document.
+    querySelector('.ParentContainer')
+    ParentContainer.innerHTML = [`<img class= pic2 src="https://m.media-amazon.com/images/M/MV5BMTk0MDI4ODk5NF5BMl5BanBnXkFtZTcwMzg3ODQ3MQ@@._V1_UY317_CR2,0,214,317_AL_.jpg">`,
+    `<img class= "pic2" src="https://m.media-amazon.com/images/M/MV5BMjljYTUwNGMtMjhhMC00MzY3LWEwNjQtMjBmMzJhM2ViNmExXkEyXkFqcGdeQXVyMTc5Mjc5OTc@._V1_UY317_CR21,0,214,317_AL_.jpg"> `]
+}
+const printToscreen3 = () => {
+    let ParentContainer = document.
+    querySelector('.ParentContainer')
+    ParentContainer.innerHTML = [`<img class= pic3 src="https://m.media-amazon.com/images/M/MV5BMjI3ODkxMjU3OF5BMl5BanBnXkFtZTgwMTk2Njk3MTE@._V1_UX214_CR0,0,214,317_AL_.jpg">`,
+    `<img class= "pic3" src="https://m.media-amazon.com/images/M/MV5BMTY1Njc5MzE1OF5BMl5BanBnXkFtZTcwMTc1NDM4Nw@@._V1_UY317_CR3,0,214,317_AL_.jpg"> `]
+}
+
